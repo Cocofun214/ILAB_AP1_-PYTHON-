40e39831079fbab7a0ddfe8639d1b1797103a825
-----J---- <108605984+Cocofun214@users.noreply.github.com>
2022-11-14 08:21:13 +0700

    Update TOPIK5.py

--- TOPIK5.py
+++ TOPIK5.py
@@ -188,3 +188,129 @@ main()
 ------------- Prelab 5.4 ------------------
 
 
+== 1 == 
+
+BandungJakartaMedanSurabaya
+
+== 2 ==
+
+Program menghasilkan error
+
+
+== 3 ==
+
+def main():
+    angka = [234, 694, 123, 789, 923, 674, 534]
+    with open('list_angka.txt', 'w') as x:
+        for a in angka:
+            x.write(str(a) + '\n')
+main()
+
+
+== 4 ==
+def main():
+    rata_rata = 0.0
+    nilai_tertinggi = 0.0
+    nilai_terendah = 0.0
+    number_list = []
+    with open('daftar_nilai.txt', 'r') as fp:
+        number_list = [float(item) for item in fp.readlines()]
+    nilai_terendah = min(number_list)
+    nilai_tertinggi = max(number_list)
+    rata_rata = sum(number_list) / len(number_list)
+    x = "{:.2f}".format(rata_rata)
+    print('Rata-rata nilai ujian: ', x)
+    print('Nilai ujian tertinggi: ', nilai_tertinggi)
+    print('Nilai ujian terendah: ', nilai_terendah)
+main()
+
+
+
+----------- Prelab 5.5 -------------
+
+
+== 1 ==
+
+
+def main():
+    x = int(input('Berapa banyak record nilai mahasiswa yang ingin Anda tambahkan? '))
+    with open('nilai_mahasiswa.txt', 'a') as nilai:
+        for i in range(1, x +1):
+            print(f'Masukkan record nilai mahasiswa ke {i}')
+            nama = input('Nama: ')
+            Nilai = input('Nilai: ')
+            nilai.write(nama + '\n')
+            nilai.write(Nilai + '\n')
+            print()
+main()
+
+
+== 2 ==
+
+def main():
+    with open('nilai_mahasiswa.txt', 'r') as d:
+        data = d.readline()
+        while data !='':
+            Nilai = d.readline()
+            nama = data.rstrip('\n')
+            nilai = Nilai.rstrip('\n')
+            print(f'Nama: {nama}')
+            print(f'Nilai: {nilai}')
+            print()
+            data = d.readline()
+main()
+
+
+
+---------- Prelab 5.6 ---------
+
+
+== 1 ==
+
+Kode ini menyebabkan ValueError.
+
+
+== 2 ==
+
+Sebuah error terjadi.
+
+
+== 3 ==
+
+
+def main():
+    try:
+        x = int(input('Masukkan sebuah angka yang akan dibagi: '))
+        y = int(input('Masukkan sebuah angka pembagi: '))
+        print(f'{x} dibagi dengan {y} sama dengan {x/y}')
+    except ValueError:
+        print('Nilai yang diinput salah.')
+    except ZeroDivisionError:
+        print('Tidak dapat membagi dengan nol.')
+main()
+
+ 
+    
+== 4 ==
+
+
+def main():
+    data = []
+    d = input('Masukkan nama file: ')
+    try:
+        dim = open(d, 'r')
+        for i in dim:
+            y = float(i)
+            data.append(y)
+    except FileNotFoundError:
+        print('File tidak ditemukan.')
+    except ValueError:
+        print('Terdapat data non numerik dalam file.')
+    else:
+        print('Rata-rata nilai:', round(sum(data) / len(data), 2))
+        print('Nilai tertinggi:', max(data))
+        print('Nilai terendah:', min(data))
+main()
+
+
+
