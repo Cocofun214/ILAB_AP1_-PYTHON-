24bf7cf8e7aef6ca2714fc4165c7c05447a40851
-----J---- <108605984+Cocofun214@users.noreply.github.com>
2022-11-17 00:06:10 +0700

    Update TOPIK5.py

--- TOPIK5.py
+++ TOPIK5.py
@@ -314,3 +314,73 @@ main()
 
 
 
+=================== ACT 5 ===================
+
+
+# Program ini membaca record nilai mahasiswa
+# dari file nilai_mahasiswa.txt
+def main():
+
+    # Variabel sebagai penanda jika nama mahasiswa yang dicari ditemukan
+    found = False
+    
+    try:
+        # [1] Tuliskan statement untuk meminta nama file ke pengguna
+        # Gunakan prompt: 'Masukkan nama file: '
+        nama_file = input('Masukkan nama file: ')
+
+        # [2] Tuliskan staement untuk membuka file dengan nama file yang diberikan pengguna
+        # Anda dapat menggunakan statement with atau 3 langkah menggunakan file: buka, proses, tutup.
+        with open(nama_file, 'r') as output_file:
+        
+            # [3] Tampilkan pesan: "File <nama_file> berhasil dibuka.". Tambahkan baris baru.
+            print('File', nama_file, 'berhasil dibuka.\n')
+            
+            # [4] Tuliskan statement untuk meminta nama mahasiswa yang ingin dicari
+            # Gunakan prompt: 'Masukkan nama mahasiswa yang ingin dicari: '
+            nama_mahasiswa = input('Masukkan nama mahasiswa yang ingin dicari: ')
+
+            # [5] Tuliskan statement untuk membaca baris pertama dari file (nama mahasiswa).
+            output = output_file.readline()
+            
+            # [6] Tuliskan loop while yang mengiterasi baris-baris file
+            while output != '':
+            
+                # [7] Tuliskan statement untuk membaca nilai mahasiswa
+                nilai = output_file.readline()
+                
+                # [8] Tuliskan statement untuk menghilangkan baris baru pada nama mahasiswa dan nilai mahasiswa
+                # Gunakan rstrip.
+                output = output.rstrip('\n')
+                nilai = nilai.rstrip('\n')
+                
+                # [9] Tuliskan struktur seleksi yang menentukan apakah nama mahasiswa cocok
+                # dengan nama yang ingin dicari
+                if nama_mahasiswa == output:
+                
+                    # [10] Jika nama_mahasiswa sama dengan nama yang ingin dicari
+                    # Tampilkan Nama dan Nilai
+                    print(f'Nama Mahasiswa: {output}')
+                    print(f'Nilai: {nilai}')
+
+                    # [11] Tetapkan nilai variabel penanda found dengan True
+                    # Ini karena mahasiswa ditemukan
+                    found = True
+
+                # [12] Tuliskan statement membaca baris berikutnya
+                output = output_file.readline()
+
+    # [13] Tuliskan klausa except FileNotFoundError
+    # Pada body klausa except ini tampilkan: File <nama_file> tidak ditemukan.
+    except FileNotFoundError:
+        print('File', nama_file, 'tidak ditemukan.')
+        
+    # [14] Tuliskan klausa else
+    # Pada body klausa else tuliskan struktur seleksi jika found tidak sama dengan True
+    # tampilkan pesan: "Nama <nama yang dicari> tidak ditemukan."
+    except:
+        print('Nama', nama_mahasiswa, 'tidak ditemukan.')
+                    
+# Panggil fungsi main
+main()
+
